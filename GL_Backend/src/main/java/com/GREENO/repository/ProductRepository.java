fc37e4ed37dd4987636feaf6ca3dfd0e473ba745
Nada Himdi <nadahimdii@gmail.com>
2024-10-27 15:08:44 +0100

    deleted old version

--- GL_Backend/src/main/java/com/GREENO/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.GREENO.repository;
-
-import com.GREENO.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
--- GL_Backend/src/main/java/com/agropharm/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agropharm.repository;
-
-import com.agropharm.Entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
