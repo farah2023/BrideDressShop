0edef967d4198c5ed8b32f1aafc0a1da143b7af3
Nada Himdi <nadahimdii@gmail.com>
2024-10-31 13:57:14 +0100

    deleted old version

--- Backend/src/main/java/com/agropharm/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agropharm.repository;
-
-import com.agropharm.Entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-
-}
